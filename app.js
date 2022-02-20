582b20abc8f9154659a64147c039128a67b36dc3
Magnus <752zombie@gmail.com>
2022-02-20 17:27:41 +0100

    test

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 })
 
-app.listen(80, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
