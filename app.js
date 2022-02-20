42b519916ae01a3176b1d1a9f0c724b910eccc39
Magnus <752zombie@gmail.com>
2022-02-20 17:07:21 +0100

    test commit

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 })
 
-app.listen(8080, () => console.log("Server is running on port 8080"));
+app.listen(80, () => console.log("Server is running on port 80"));
