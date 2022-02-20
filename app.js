eb582742c3112b968df56ac3ab211ec8e1e20b6c
Magnus <752zombie@gmail.com>
2022-02-20 17:11:27 +0100

    test commit

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 })
 
-app.listen(80, () => console.log("Server is running on port 80"));
+app.listen(80, () => console.log(`Server is running on port ${PORT}`));
